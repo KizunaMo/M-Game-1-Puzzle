62c7f5d55a47492b02750d9a3b10b7f2764b364b
BoQuan <mo791206@gmail.com>
2021-06-07 17:43:29 +0800

    測試

---
+++ Grid system/Testing.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    Grid grid;
+    int value;
+    private void Start()
+    { 
+        
+        grid = new Grid(9, 1, 2.65f, new Vector3(0, 0));
+        grid = new Grid(10, 1, 2.65f, new Vector3(4, 4));
+        grid = new Grid(11, 1, 2.65f, new Vector3(8, 8));
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(Camera.main.ScreenToWorldPoint(Input.mousePosition), 1);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+        }
+    }
+}
